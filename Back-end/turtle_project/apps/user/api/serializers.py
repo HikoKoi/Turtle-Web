1f24ec8ac4ecdbd4f53fb1e8940520a43e5e3e29
linhphung211 <156571443+linhphung211@users.noreply.github.com>
2026-04-14 00:18:51 +0700

    [13/04/2026]: Completed backend for user and lesson
    * [13/04/2026]: Completed backend for user and lesson
    
    * [14/04/2026]: completed config file

--- Back-end/turtle_project/apps/user/api/serializers.py
+++ Back-end/turtle_project/apps/user/api/serializers.py
@@ -1,3 +1,18 @@
+# from rest_framework import serializers
+# from ..models import User, UserSession
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'full_name', 'username', 'email', 'phone_number', 'avatar', 'birthday']
+#         # Không trả về password trong API
+
+# class UserSessionSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserSession
+#         fields = ['session_id', 'ip_address', 'user_agent', 'is_revoked', 'created_at', 'expired_at']
+
+
 from rest_framework import serializers
 from django.core.validators import RegexValidator, EmailValidator
 from django.contrib.auth.password_validation import validate_password
